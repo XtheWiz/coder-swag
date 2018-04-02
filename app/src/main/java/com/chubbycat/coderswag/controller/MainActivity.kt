ae89d5f3a36adf1087d09057c3ac5a014157f6ea
intw_wiruj <xthewiz@Wirujs-MacBook-Pro.local>
2018-04-02 23:48:56 +0700

    lesson-02

--- app/src/main/java/com/chubbycat/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/chubbycat/coderswag/controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.chubbycat.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.chubbycat.coderswag.R
+import com.chubbycat.coderswag.model.Category
+import com.chubbycat.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+        listviewCategory.adapter = adapter
     }
 }
